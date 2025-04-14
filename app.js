b0b7ec09d9e2feddf82a0c5aad7d8a7749c953ce
Preetham Prasad <preethamprasad1674@gmail.com>
2025-04-14 11:59:42 +0530

    testing workflows

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  //Intentionally vulnerable: No input validation or authentication check
+  //Intentionally vulnerable: No input validation or authentication check 1
   if (username && password) {
     res.send(`<h1>Welcome, ${username}</h1>`);
   } else {
